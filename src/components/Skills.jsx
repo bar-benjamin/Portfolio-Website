686f0164ebb2762eee098098f730eef524e7dafd
bar-benjamin <bar.benjamin123@gmail.com>
2023-09-11 14:06:07 +0300

    updated technologies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
-import JAVA from '../assets/java-logo.png';
 import PYTHON from '../assets/python-logo.png';
+import JAVASCRIPT from '../assets/javascript-logo.png';
+import REACT from '../assets/react-logo.png'
+import NODEJS from '../assets/nodejs-logo.png'
+import GCP from '../assets/gcp-logo.png'
+import JAVA from '../assets/java-logo.png';
 import C from '../assets/c-logo.png';
 import SPRING from '../assets/spring-logo.png';
 import GRADLE from '../assets/gradle-logo.png';
@@ -23,19 +27,38 @@ const Skills = () => {
               <br></br>
               <br></br>
           </div>
-
           <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={PYTHON} alt="PYTHON icon" />
+                  <p className='my-4'>PYTHON</p>
+              </div>    
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JAVASCRIPT} alt="JAVASCRIPT icon" />
+                  <p className='my-4'>JAVASCRIPT</p>
+              </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={JAVA} alt="JAVA icon" />
                   <p className='my-4'>JAVA</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={PYTHON} alt="PYTHON icon" />
-                  <p className='my-4'>PYTHON</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={C} alt="C icon" />
                   <p className='my-4'>C</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={REACT} alt="REACT icon" />
+                  <p className='my-4'>REACT</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={NODEJS} alt="NODEJS icon" />
+                  <p className='my-4'>NODEJS</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={GCP} alt="GCP icon" />
+                  <p className='my-4'>Google Cloud</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={GIT} alt="GIT icon" />
+                  <p className='my-4'>GIT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={SPRING} alt="SPRING icon" />
@@ -52,10 +75,6 @@ const Skills = () => {
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={MONGODB} alt="MONGODB icon" />
                   <p className='my-4'>MONGODB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={GIT} alt="GIT icon" />
-                  <p className='my-4'>GIT</p>
               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={DOCKER} alt="DOCKER icon" />
